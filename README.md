60d32dc20da616b3c5cd3e411304afbf596a64d1
melanieprimpas <melanie.primpas@gmail.com>
2024-09-16 22:18:21 -0400

    made updates to all files, still does not run properly need to continue working on weatherService.ts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherDashboard
+
+const APIKey = "268ed720022aa52492630f0fce7f0747"
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,32 @@
+import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
-import express from 'express';
-dotenv.config();
-
+import path from 'node:path';
+import { fileURLToPath } from 'node:url'; 
 // Import the routes
 import routes from './routes/index.js';
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
-
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
+// TODO: Serve static files of entire client dist folder
+app.use(express.static('../client/dist'));
+
+// Connect routes
 app.use(routes);
+
+app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
+});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
